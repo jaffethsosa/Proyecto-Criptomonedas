ecbde789e1107425320bf02f6e9d052d9d31a4c7
Jaffeth Sosa <jaffeth_sosa@hotmail.com>
2024-11-08 14:42:42 -0600

    add style login

--- src/components/usuarios/Login.jsx
+++ src/components/usuarios/Login.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
 
@@ -12,7 +13,7 @@ const Login = () => {
     })
 
     const submit = (e) => {
-        e.preventDefault(); // Evita que el formulario envíe la solicitud de forma predeterminada
+        e.preventDefault(); 
         axios.post('https://reqres.in/api/login', user)
             .then(data => {
                 localStorage.setItem("tokenCriptoSOSA", data.data.token)
@@ -24,26 +25,33 @@ const Login = () => {
     if(localStorage.getItem("tokenCriptoSOSA")) return <Navigate to="/"/>
 
     return(
+        <div className="title">
+            <h1>Bienvenido a CriptoSOSA</h1>
         <div className="login-container">
-            <h1>Inciciar sesion </h1>
+            <h1>Iniciar sesión </h1>
             <form onSubmit={submit}>
                 <div className="field">
                     <label htmlFor="email">Correo Electrinico</label>
+                    <p/>
                     <input required onChange={(e) => {
                         setUser({ ...user, email: e.target.value })
                     }} type="email" name="email" />
                 </div>
                 <div className="field">
                     <label htmlFor="password">Contraseña</label>
-                </div>
+                    <p/>
                     <input required onChange={(e)=>{
                         setUser({ ...user, password: e.target.value })
                     }} type="password" name="password" />
+                </div>
+                
+                    
                 <div className="submi">
                     <input type="submit" value="Ingresar" />
                 </div>
             </form>
         </div>
+    </div>
     )
 }
 
